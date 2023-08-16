14dc0036479b2cdb2547730f4b51e296245a6111
Andrej <137786054+DujMaa@users.noreply.github.com>
2023-08-16 20:58:30 +0200

    modified: Add function for one round play

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-let signs = ["Rock", "Paper", "Scissors"]
+let signs = ["rock", "paper", "scissors"]
 
 let randNum = Math.floor(Math.random() * 3); 
 
+
 function getComputerChoice(){
-    console.log(signs[randNum]);
+    return signs[randNum];
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() == "rock"){
+        if(computerSelection == "paper")
+            return `You Lose! Paper beats Rock`;
+        else if(computerSelection == "scissors")
+                return `You Win! Rock beats Scissors`;
+        else if(computerSelection == "rock")
+                return `It is Draw!`;
+    }else if(playerSelection.toLowerCase() == "paper"){
+        if(computerSelection == "paper")
+            return `It is Draw!`;
+        else if(computerSelection == "scissors")
+                return `You Lose! Scissors beats Paper`;
+        else if(computerSelection == "rock")
+                return `You Win! Paper beats Rock`;
+    }else if(playerSelection.toLowerCase() == "scissors"){
+        if(computerSelection == "paper")
+            return `You Win! Scissors beats Paper`;
+        else if(computerSelection == "scissors")
+                return `It is Draw!`;
+        else if(computerSelection == "rock")
+                return "You Lose! Rock beats Scissors";
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log("computer: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
